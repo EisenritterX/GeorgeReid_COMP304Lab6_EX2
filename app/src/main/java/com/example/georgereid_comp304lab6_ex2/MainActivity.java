07afed0994cd4afa06eca1417ca982eb04d290c7
George Zhou <georgezhou89@hotmail.com>
2021-11-28 18:07:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/georgereid_comp304lab6_ex2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.georgereid_comp304lab6_ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
